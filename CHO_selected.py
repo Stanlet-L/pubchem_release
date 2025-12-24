8b908fc4f4a5695d9efd8b257d808e4d2e8c83eb
Evecyc <145519857+Evecyc@users.noreply.github.com>
2025-12-24 12:50:11 -0500

    rename files

--- CHO_selected.py
+++ CHO_selected.py
@@ -13,14 +13,25 @@ def is_CHO_only(formula):
     return all(elem in {"C", "H", "O"} for elem in elements)
 
 def main():
+    # file routes
+    # Case 1: Ux1 -> X
+    #input_file = "4-hydroxyphenyl.csv"
+    #output_file = "4-hydroxyphenyl_CHO.csv"
+    # Case 1: Uy11 -> Y1 & Case 2: Ux1 -> X
+    #input_file = "3,4-dihydroxyphenyl.csv"
+    #output_file = "3,4-dihydroxyphenyl_CHO.csv"
+    # Case 1: Uy22 -> Y2
+    # input_file = "1,2-diol.csv"
+    # output_file = "1,2-diol_CHO.csv"
+    # Case 2: Uy1 -> Y1
+    # input_file = "4-hydroxy-3-methoxyphenyl.csv"
+    # output_file = "4-hydroxy-3-methoxyphenyl_CHO.csv"
+    # Case 2: Uy2 -> Y2
     input_file = "3-hydroxy-4-methoxyphenyl.csv"
     output_file = "3-hydroxy-4-methoxyphenyl_CHO.csv"
 
     df = pd.read_csv(input_file)
-
     df_filtered = df[df["Molecular_Formula"].apply(is_CHO_only)]
-
-    print(f"Output as: {output_file}")
     df_filtered.to_csv(output_file, index=False)
 
 if __name__ == "__main__":
